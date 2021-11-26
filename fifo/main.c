bd2ef4b7377395f6dbbc3251c4de583a0add5ae3
SaintAleksei <danilenko.aa@phystech.edu>
2021-11-26 19:26:29 +0300

    Cosmetic fix in fifo/main.c

--- fifo/main.c
+++ fifo/main.c
@@ -13,11 +13,11 @@
 #define NAMESIZE 0x40
 #define TIMEOUT 3
 
-#define CHECK_ERROR(message, condition)\
+#define ERR(condition)\
     if (condition)\
     {\
-        fprintf (stderr, "%s:%u:%s: ", __FILE__, __LINE__, __PRETTY_FUNCTION__);\
-        perror (message);\
+        fprintf (stderr, "%s:%u:%s: %s\n", __FILE__, __LINE__,\
+                 __PRETTY_FUNCTION__, strerror (errno) );\
         exit (EXIT_FAILURE);\
     }
 
@@ -46,18 +46,18 @@ int main (int argc, char *argv[])
         while (1)
         {
             bytes_count = read (fd_fifo, buf, BUFSIZE);
-            CHECK_ERROR ("Can't read data from fifo file", bytes_count == -1);
+            ERR (bytes_count == -1);
 
             if (!bytes_count)
             {
                 if (no_sender)
                     fprintf (stderr, "No sender\n");
-                CHECK_ERROR ("Can't remove fifo file", unlink (fifo_name) == -1);
+                ERR (unlink (fifo_name) == -1);
                 exit (EXIT_SUCCESS);
             }
 
             bytes_count = write (STDOUT_FILENO, buf, bytes_count);
-            CHECK_ERROR ("Can't write data to fifo file", bytes_count == -1);
+            ERR (bytes_count == -1);
 
             no_sender = 0;
         }
@@ -65,7 +65,7 @@ int main (int argc, char *argv[])
     else /* Sender */
     {
         int fd_input = open (argv[1], O_RDONLY);
-        CHECK_ERROR ("Can't open input file", fd_input == -1);
+        ERR (fd_input == -1);
 
         int fd_fifo = find_reciever ();
         
@@ -74,26 +74,26 @@ int main (int argc, char *argv[])
         while (1)
         {
             bytes_count = read (fd_input, buf, BUFSIZE);
-            CHECK_ERROR ("Can't read data from file", bytes_count == -1);
+            ERR (bytes_count == -1);
 
             if (!bytes_count)
                 exit (EXIT_SUCCESS);
 
             bytes_count = write (fd_fifo, buf, bytes_count);
-            CHECK_ERROR ("Can't write data to fifo file", bytes_count == -1);
+            ERR (bytes_count == -1);
         }
     }
 }
 
 int find_reciever () /* Sender */
 {
-    CHECK_ERROR ("Can't make common fifo file", mkfifo (".file.fifo", S_IWUSR | S_IRUSR) && errno != EEXIST);
+    ERR (mkfifo (".file.fifo", S_IWUSR | S_IRUSR) && errno != EEXIST);
 
     int fd_common_fifo = open (".file.fifo", O_RDONLY);
-    CHECK_ERROR ("Can't open common fifo file", fd_common_fifo == -1);
+    ERR (fd_common_fifo == -1);
 
     ssize_t nbytes = read (fd_common_fifo, fifo_name, NAMESIZE);
-    CHECK_ERROR ("Can't read name from common fifo", nbytes == -1);
+    ERR (nbytes == -1);
 
     if (nbytes == 0) 
     {
@@ -103,9 +103,9 @@ int find_reciever () /* Sender */
     }
 
     int fd_fifo = open (fifo_name, O_WRONLY | O_NONBLOCK);
-    CHECK_ERROR ("Can't open fifo file", fd_fifo == -1);
+    ERR (fd_fifo == -1);
 
-    CHECK_ERROR ("Can't set flags", fcntl (fd_fifo, F_SETFL, O_WRONLY) == -1);
+    ERR (fcntl (fd_fifo, F_SETFL, O_WRONLY) == -1);
 
     close (fd_common_fifo);
 
@@ -113,24 +113,24 @@ int find_reciever () /* Sender */
 }
 
 int find_sender () /* Reciever */
-{
-    CHECK_ERROR ("Can't make common fifo file", mkfifo (".file.fifo", S_IWUSR | S_IRUSR) && errno != EEXIST);
+
+    ERR (mkfifo (".file.fifo", S_IWUSR | S_IRUSR) && errno != EEXIST);
 
     int fd_common_fifo = open (".file.fifo", O_WRONLY);
-    CHECK_ERROR ("Can't open common fifo file", fd_common_fifo == -1);
+    ERR (fd_common_fifo == -1);
 
     pid_t fifo_pid = getpid ();
     snprintf (fifo_name, NAMESIZE, ".%u.fifo", fifo_pid);
 
-    CHECK_ERROR ("Can't make fifo file", mkfifo (fifo_name, S_IWUSR | S_IRUSR) && errno != EEXIST);
+    ERR (mkfifo (fifo_name, S_IWUSR | S_IRUSR) && errno != EEXIST);
 
     int fd_fifo = open (fifo_name, O_RDONLY | O_NONBLOCK);
-    CHECK_ERROR ("Can't open fifo file", fd_fifo == -1);
+    ERR (fd_fifo == -1);
 
-    CHECK_ERROR ("Can't set flags", fcntl (fd_fifo, F_SETFL, O_RDONLY) == -1);
+    ERR (fcntl (fd_fifo, F_SETFL, O_RDONLY) == -1);
 
     ssize_t bytes_count = write (fd_common_fifo, fifo_name, NAMESIZE);
-    CHECK_ERROR ("Can't write data to common fifo file", bytes_count == -1);
+    ERR (bytes_count == -1);
 
     close (fd_common_fifo);
 
