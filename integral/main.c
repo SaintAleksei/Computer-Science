0e1f2edc9b7efe443ae7f12e43957e50873e27e4
Aleksei Danilenko <danilenko.aa@phystech.edu>
2022-04-26 20:49:17 +0300

    Small changes. Still not working correct

--- integral/main.c
+++ integral/main.c
@@ -1,5 +1,7 @@
 #define _GNU_SOURCE
 #include <sys/sysinfo.h>
+#include <error.h>
+#include <unistd.h>
 #include <sched.h>
 #include <pthread.h>
 #include <stdint.h>
@@ -12,7 +14,7 @@
 
 #define INTEGRAL_MAXTHREADS CPU_SETSIZE
 #define INTEGRAL_START 0.0
-#define INTEGRAL_END 100.0
+#define INTEGRAL_END 500.0
 #define INTEGRAL_DX 1e-6
 
 struct thread_info
@@ -22,12 +24,11 @@ struct thread_info
     double result;
 };
 
-void *callback(void *data);
+void *thread_cb(void *data);
 
 int main(int argc, char **argv)
 {
     pthread_t thread[INTEGRAL_MAXTHREADS];
-    struct thread_info tinfo[INTEGRAL_MAXTHREADS];
     double result = 0.0;
     int retval = 0;
 
@@ -46,58 +47,68 @@ int main(int argc, char **argv)
         exit(EXIT_FAILURE);
     }
 
-    if (nthreads > INTEGRAL_MAXTHREADS)
+    unsigned long nprocs = get_nprocs();
+
+    if (nthreads > nprocs)
     {
         fprintf(stderr, "Too much threads\n");
         exit(EXIT_FAILURE);
     }
 
-    unsigned long nprocs = get_nprocs();
     pthread_attr_t attr;
 
     retval = pthread_attr_init(&attr);
     assert(retval == 0);
 
+    size_t alignment = sysconf(_SC_PAGESIZE);
+    uint8_t *buf = NULL;
+    retval = posix_memalign((void **) &buf, alignment, alignment * nprocs);
+    assert(retval == 0);
+
     double step = (INTEGRAL_END - INTEGRAL_START) / (double) nthreads;
     double start = 0.0;
-    for (unsigned long i = 0; i < nthreads; i++, start += step)
+    for (unsigned long i = 0; i < nprocs; i++, start += step)
     {
         cpu_set_t set;
 
         CPU_ZERO(&set); 
-        CPU_SET(i % nprocs, &set);
+        CPU_SET(i, &set);
 
         retval = pthread_attr_setaffinity_np(&attr, sizeof(set), &set);
         assert(retval == 0);
 
-        tinfo[i].start = start;
-        tinfo[i].end = start + step;
+        struct thread_info *tinfo = (struct thread_info *)(buf + i * alignment);
+        tinfo->start = start;
+        tinfo->end = start + step;
 
-        retval = pthread_create(thread + i, &attr, callback, tinfo + i);
+        retval = pthread_create(thread + i, &attr, thread_cb, tinfo);
         assert(retval == 0);
-
     }
 
     retval = pthread_attr_destroy(&attr);
     assert(retval == 0);
 
-    for (unsigned long i = 0; i < nthreads; i++)
+    for (unsigned long i = 0; i < nprocs; i++)
     {
         int retval = pthread_join(thread[i], NULL);
-        assert(retval == 0);
+        if (retval != 0)
+            error(EXIT_FAILURE, retval, "pthread_join");
 
-        result += tinfo[i].result;
+        if (i < nthreads)
+            result += ((struct thread_info *) (buf + i * alignment))->result;
     }
 
-    fprintf(stderr, "Result is %lg\n", result);
+    printf("Result is %lg\n", result);
+
+    free(buf);
 }
 
 double func(double x)
 {
-    return x*x + x*cos(x);
+    return cos(x);
 }
 
-void *callback(void *data)
+void *thread_cb(void *data)
 {
     struct thread_info *tinfo = data;
 
