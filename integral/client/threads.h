9985d24d3c85f5a364b8230f95bee0cf683ad2b4
Aleksei <danilenko.aa@phystech.edu>
2022-05-11 10:52:01 -0400

    Out of date file are deleted

--- integral/client/threads.h
+++
@@ -1,9 +0,0 @@
-#ifndef THREADS_H_INCLUDED
-#define THREADS_H_INCLUDED
-
-#include <stddef.h>
-
-int threads_integrate(size_t nthreads, double rangeStart, double rangeEnd,
-                      double dx, double (*func)(double x), double *result);
-
-#endif
